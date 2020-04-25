bdf3955fd4417787d7bc6c885e267e5a78beef59
Ahidul Islam <islamahidul1@gmail.com>
2020-04-25 15:10:28 +0600

    multilevel, multiple inheritance done

--- inheritence.py
+++ inheritence.py
@@ -49,3 +49,54 @@ Python also has a super() function that will make the child class
 inherit all the methods and properties from its parent:
 
 """
+
+"""
+Multi level Inheritance
+"""
+
+class A:
+    def display1(self):
+        print("in side A")
+
+class B(A):
+    # display1() already inherited from class A
+    def display2(self):
+        print("in side B")
+
+class C(B):
+    # display1() by inheritance
+    # display2() by inheritance
+    def diplay3(self):
+        print("in side C")
+
+obj = C()
+obj.display1()
+obj.display2()
+obj.diplay3()
+
+
+"""
+Multiple Inheritance
+yes, though java is single inheritance, kotlin is also
+
+python supports multiple inheritance
+"""
+
+class P:
+    def display(self):
+        print("in side P")
+
+class Q:
+    def display(self):
+        print("in side Q")
+class R(P, Q): # As P is prioritize so display method will be called from P
+    pass
+
+class S(Q, P):# But here Q is prioritize so display method will be called from Q
+    pass
+
+rObj = R()
+rObj.display()
+
+sObj = S()
+sObj.display()
